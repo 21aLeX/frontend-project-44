c9ef66367daabe8d56340d9db19a1e58e3af6f09
alex <aleksandra12mail@mail.ru>
2022-10-31 17:10:56 +0800

    neyspel

---
+++ games/progression.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+import Math from 'math';
+import index from '../src/index.js';
+
+export default function progression() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('What number is missing in the progression?');
+  let countAnswer = 0;
+  let correctAnsewr = '';
+  while (countAnswer !== 3) {
+    const array = [Math.floor(Math.random() * 100)];
+    const sizeArray = Math.floor(Math.floor(Math.random() * (10 - 5 + 1)) + 5);
+    const step = Math.floor(Math.floor(Math.random() * 10));
+    const element = Math.floor(Math.floor(Math.random() * (sizeArray + 1)));
+    let strQuestion = '';
+    console.log(sizeArray);
+    for (let i = 1; i < sizeArray; i += 1) {
+      array.push(array[i - 1] + step);
+      if (i === element) {
+        correctAnsewr = array[i];
+        strQuestion += `.. `;
+      } else {
+        strQuestion += `${array[i]} `;
+      }
+    }
+
+    //     const strQuestion = `${number1} ${number2}`;
+    //     correctAnsewr = nodFinding(number1, number2);
+    //     const indexAnswer = index(correctAnsewr, strQuestion);
+    //     if (indexAnswer === 1) {
+    //       countAnswer += 1;
+    //     } else {
+    //       return `${indexAnswer}${name}!`;
+    //     }
+  }
+  return `Congratulations, ${name}!`;
+}
