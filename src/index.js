520660b143db891b2615a9ce2409f6cdd511ef3e
Alex <aleksandra12mail@mail.ru>
2022-11-15 19:53:19 +0800

    second check№4

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ export default function template(generationData, name) {
   for (let i = 0; i < 3; i += 1) {
     const answers = generationData();
     const [correctAnsewr, strQuestion] = answers;
-    if (correctAnsewr === 'Error operation') {
-      throw 'Error operation';
-    }
 
     console.log(`Question: ${strQuestion}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === String(correctAnsewr)) {
       console.log('Correct!');
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnsewr}.`);
+      console.log(
+        `${answer} is wrong answer ;(. Correct answer was ${correctAnsewr}.`
+      );
       console.log(`Let's try again, ${name}!`);
       return;
     }
